0a7d450a09d11115abd554306e69335b3d312797
adithyamca <93462851+adithyamca@users.noreply.github.com>
2022-04-06 11:35:39 +0530

    first commit

---
+++ reverse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class reverse
+{
+   public static void main(String[] args)
+   {
+      int num, rem, rev=0;
+      Scanner s = new Scanner(System.in);
+      
+      System.out.print("Enter a Number: ");
+      num = s.nextInt();
+      
+      while(num!=0)
+      {
+         rem = num%10;
+         rev = (rev*10) + rem;
+         num = num/10;
+      }
+      
+      System.out.println("\nReverse = " +rev);
+   }
+}
